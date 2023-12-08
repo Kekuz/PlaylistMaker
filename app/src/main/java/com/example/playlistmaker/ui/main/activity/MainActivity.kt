fdcb229493a9110c944c424962a9db87cd7f0f48
Kekuz <anton.edlenko@yandex.ru>
2023-12-08 04:19:24 +0300

    Media activity refactored to clean architecture.

--- app/src/main/java/com/example/playlistmaker/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/main/activity/MainActivity.kt
@@ -3,19 +3,12 @@ package com.example.playlistmaker.ui.main.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
-import com.example.playlistmaker.R
-import com.example.playlistmaker.creator.Creator
 import com.example.playlistmaker.databinding.ActivityMainBinding
-import com.example.playlistmaker.domain.search.models.Track
-import com.example.playlistmaker.ui.audioplayer.view_model.AudioPlayerViewModel
 import com.example.playlistmaker.ui.main.view_model.MainViewModel
 import com.example.playlistmaker.ui.media.activity.MediaActivity
 import com.example.playlistmaker.ui.search.activity.SearchActivity
 import com.example.playlistmaker.ui.settings.activity.SettingsActivity
-import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
