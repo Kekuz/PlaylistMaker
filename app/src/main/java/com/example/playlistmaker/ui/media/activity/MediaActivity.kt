ded0f9ce351bec8d6b4ebcf87728303305307c9e
Kekuz <anton.edlenko@yandex.ru>
2023-12-14 19:16:34 +0300

    added viewPager in MediaActivity; Added 2 tabs to viewPager

--- app/src/main/java/com/example/playlistmaker/ui/media/activity/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/media/activity/MediaActivity.kt
@@ -3,18 +3,40 @@ package com.example.playlistmaker.ui.media.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaBinding
 import com.example.playlistmaker.ui.media.view_model.MediaViewModel
 import com.example.playlistmaker.ui.search.view_model.SearchViewModel
+import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MediaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMediaBinding
+    private lateinit var tabMediator: TabLayoutMediator
     private val viewModel by viewModel<MediaViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMediaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.pager.adapter = MediaPagerAdapter(supportFragmentManager, lifecycle)
+        binding.tabLayout.tabRippleColor = null
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            when(position) {
+                0 -> tab.text = "Избранные треки"
+                1 -> tab.text = "Плейлисты"
+            }
+        }
+
+        tabMediator.attach()
+
+        binding.ivBackArrowBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 }
